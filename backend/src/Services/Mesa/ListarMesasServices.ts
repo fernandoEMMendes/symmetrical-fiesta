31be41726f59dcf67e440ec7e46e9b464ed308a3
Cappucino <fernandoeduardomottamendes@gmail.com>
2024-03-04 13:51:33 -0300

    04/03/2024

--- backend/src/Services/Mesa/ListarMesasServices.ts
+++ backend/src/Services/Mesa/ListarMesasServices.ts
@@ -2,7 +2,11 @@ import prismaClient from "../../prisma"
 
 class ListarMesaServices {
     async execute() {
-        const mesas = await prismaClient.mesa.findMany({})
+        const mesas = await prismaClient.mesa.findMany({
+            orderBy: {
+                numero_mesa: "asc"
+            }
+        })
         return (mesas)
     }
 }
