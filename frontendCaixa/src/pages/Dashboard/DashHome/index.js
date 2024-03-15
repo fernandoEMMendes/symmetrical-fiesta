5a6986fc1c15dc71f5bcc8dd340879730d829b40
LUCAS CLAUDINO NASCIMENTO <lucas.cnascimento9@senacsp.edu.br>
2024-03-15 16:44:03 -0300

    cadastro de funcionario em funçoes administrativas e botao logoff no dashboard

--- frontendCaixa/src/pages/Dashboard/DashHome/index.js
+++ frontendCaixa/src/pages/Dashboard/DashHome/index.js
@@ -1,30 +1,113 @@
 import './Dashboard.css'
-import {Link} from 'react-router-dom'
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import Modal from 'react-modal'
 
 import ImgPedidos from '../DashMultimidia/pedidos.png'
 import ImgProdutos from '../DashMultimidia/Produtos.png'
+import { Flip, toast } from 'react-toastify'
+import apiLocal from '../../../APIs/apiLocal'
 
 
-export default function Dashboard(){
-    return(
+export default function Dashboard() {
+    const lucas = useNavigate()
+
+    const [modalAberto, setModalAberto] = useState(false)
+    const [nome, setNome] = useState('')
+    const [senha, setSenha] = useState('')
+
+
+    function abrirModal() {
+        setModalAberto(true)
+    }
+    function fecharModal() {
+        setModalAberto(false)
+    }
+
+    async function CadastroCachorroLoko(e) {
+        e.preventDefault()
+        if (!nome || !senha) {
+            toast.warning('Existe Campos em Branco')
+        }
+        try {
+            await apiLocal.post('/CriarUsuario', {
+                nome,
+                senha
+            })
+            setNome('')
+            setSenha('')
+            setModalAberto(false)
+            toast.success('Cadastrado com sucesso',{
+                transition: Flip,
+                autoClose:1250,
+                closeOnClick:true,
+                pauseOnHover:false,
+                draggable:false
+            })
+        } catch (err) {
+            
+            return
+        }
+    }
+
+
+function Sair() {
+    localStorage.removeItem("@PJI2024")
+    lucas('/')
+    toast.info("Logoff realizado com sucesso")
+}
+
+    return (
         <section id='header_Principal'>
             <header id='dash_Header'>
-                    <h1>Dashboard</h1>
-                </header>
+                <h1>Dashboard</h1>
+                <button onClick={Sair}>
+                    Logoff
+                </button>
+            </header>
             <main id='corpo_Principal'>
-                
+
                 <article className='corpo_Filho'>
-                <Link to='../DashPedidos' ><img className='ImgDash' src={ImgPedidos} alt='dashPedidosImg'/></Link>
-                </article>
-                
-                <article className='corpo_Filho'>
-                <Link to='../DashProdutos'><img className='ImgDash' src={ImgProdutos} alt='dashProdutosImg'/></Link>
+                    <Link to='../DashPedidos' ><img className='ImgDash' src={ImgPedidos} alt='dashPedidosImg' /></Link>
                 </article>
 
-                
+                <article className='corpo_Filho'>
+                    <Link to='../DashProdutos'><img className='ImgDash' src={ImgProdutos} alt='dashProdutosImg' /></Link>
+                </article>
+
+
             </main>
-            <footer id='dashFooter'>
-                    <h2 id='dashH2'>Administrativo</h2>
+            <footer>
+                <button id='dashFooter' className='dashFooter' onClick={abrirModal}>
+
+                    <h2 id='dashH2'>Funções Administrativas</h2>
+                </button>
+                <Modal isOpen={modalAberto} >
+                    <h2>CADASTRAR FUNCIONÁRIO</h2>
+                    <form onSubmit={CadastroCachorroLoko}>
+                        <label>Nome:</label>
+                        <input type="text"
+                            placeholder='Nome Funcionario'
+                            value={nome}
+                            onChange={(e) => setNome(e.target.value)}
+                            /><br />
+                        <label>Senha:</label>
+                        <input type="text"
+                            placeholder='Senha Funcionario' 
+                            value={senha}
+                            onChange={(e) => setSenha(e.target.value)}
+                            /><br />
+                        <button type='submit'>Cadastrar</button>
+
+
+
+
+
+
+                    </form>
+
+                    <button onClick={fecharModal} >Sair</button>
+                </Modal>
             </footer>
         </section>
     )
--- frontendCaixa/src/pages/Dashboard/DashProdutos/index.js
+++ frontendCaixa/src/pages/Dashboard/DashProdutos/index.js
@@ -1,6 +1,6 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import { toast } from 'react-toastify'
-import {ImCross} from 'react-icons/im'
+import { ImCross } from 'react-icons/im'
 import { DashListarProdutos } from '../DashHome/DashListarProdutos/DashListarProdutos'
 import Modal from 'react-modal'
 import apiLocal from '../../../APIs/apiLocal'
@@ -8,136 +8,136 @@ import './DashProdutos.css'
 
 
 
-export default function CriarProdutos(){
+export default function CriarProdutos() {
 
-    const [ nome, setNome]= useState('')
-    const [ descricao, setDescricao]= useState('')
-    const [ preco, setPreco]= useState('')
-    const [ imagem, setImagem]= useState(null)
+    const [nome, setNome] = useState('')
+    const [descricao, setDescricao] = useState('')
+    const [preco, setPreco] = useState('')
+    const [imagem, setImagem] = useState(null)
 
 
 
-    function handleImagem(e){
-        if(!e.target.files){
+    function handleImagem(e) {
+        if (!e.target.files) {
             return
         }
         const image = e.target.files[0]
-        if(image.type === 'image/png' || image.type === 'image/jpeg'){
+        if (image.type === 'image/png' || image.type === 'image/jpeg') {
             setImagem(image)
         }
     }
 
-        async function handleCadastrar(e){
-            try {
-                e.preventDefault()
-                const data = new FormData()
+    async function handleCadastrar(e) {
+        try {
+            e.preventDefault()
+            const data = new FormData()
 
-                data.append('nome',nome)
-                data.append('descricao',descricao)
-                data.append('file', imagem)
-                data.append('preco',preco)
+            data.append('nome', nome)
+            data.append('descricao', descricao)
+            data.append('file', imagem)
+            data.append('preco', preco)
 
-                await apiLocal.post('/CriarProduto', data, {
+            await apiLocal.post('/CriarProduto', data, {
 
-                })
-                toast.success("Produto Cadastrados com sucesso")
-            }
-            catch (err){
-                toast.error('Campos em brancos!')
-            }
-            setNome('')
-            setDescricao('')
-            setImagem(null)
-            setPreco('')
-        } 
-
-        const [ modalAberto, setModalAberto ] = useState(false)
-
-        function abrirModal(){
-            setModalAberto(true)
+            })
+            toast.success("Produto Cadastrados com sucesso")
         }
+        catch (err) {
+            toast.error('Campos em brancos!')
+        }
+        setNome('')
+        setDescricao('')
+        setImagem(null)
+        setPreco('')
+    }
 
-        function fecharModal(){
-            setModalAberto(false)
-        }   
+    const [modalAberto, setModalAberto] = useState(false)
+
+    function abrirModal() {
+        setModalAberto(true)
+    }
+
+    function fecharModal() {
+        setModalAberto(false)
+    }
 
 
 
-    return(
+    return (
         <section>
             <header id='dash_Produtos'>
-                    <h1>Dashboard</h1>
-                    <button onClick={abrirModal}>Adicionar Produto</button>
+                <h1>Dashboard</h1>
+                <button onClick={abrirModal}>Adicionar Produto</button>
             </header>
             <div className='containerApp'>
-            <Modal
-            className='Modal'
-            overlayClassName='overlay'
-            isOpen={modalAberto}
-            >
-                <ImCross  id='button_ModalDel'onClick={fecharModal}/>
-            <main id='main_Modal'>
-            <div id='header_Modal_AddProdutos'>
-                <h1>Adicionar produtos</h1>
+                <Modal
+                    className='Modal'
+                    overlayClassName='overlay'
+                    isOpen={modalAberto}
+                >
+                    <ImCross id='button_ModalDel' onClick={fecharModal} />
+                    <main id='main_Modal'>
+                        <div id='header_Modal_AddProdutos'>
+                            <h1>Adicionar produtos</h1>
+                        </div>
+                        <div>
+                            <form id='form_Modal_AddProduto' onSubmit={handleCadastrar}>
+                                <div id='form_Modal_LabelInputs'>
+
+                                    <div id='form_Modal_PrincipalLabel'>
+                                        <label><p>Nome:</p></label><br />
+
+                                        <label><p>Descricao:</p></label><br />
+
+                                        <label><p>Preço:</p></label><br />
+
+                                        <label><p>Imagem:</p></label><br />
+                                    </div>
+
+
+
+                                    <div id='form_Modal_PrincipalInput'>
+                                        <input className='input_Modal_AddProduto'
+                                            type='text'
+                                            value={nome}
+                                            onChange={(e) => setNome(e.target.value)}
+                                        /><br />
+
+                                        <input className='input_Modal_AddProduto'
+                                            type='text'
+                                            value={descricao}
+                                            onChange={(e) => setDescricao(e.target.value)}
+                                        /><br />
+
+                                        <input className='input_Modal_AddProduto'
+                                            type='number'
+                                            value={preco}
+                                            onChange={(e) => setPreco(e.target.value)}
+                                        />
+                                        <br />
+
+                                        <input className='input_Modal_AddProduto'
+                                            type='file'
+                                            accept='image/jpeg, image/png'
+                                            onChange={handleImagem}
+                                        /><br />
+
+                                    </div>
+                                </div>
+                                <button
+                                    type='submit' id='button_ModalAdd'
+                                >Adicionar Produto</button>
+
+                            </form>
+                        </div>
+                    </main>
+
+                </Modal>
             </div>
             <div>
-            <form id='form_Modal_AddProduto'onSubmit={handleCadastrar}>
-                <div id='form_Modal_LabelInputs'>
-                
-                <div id='form_Modal_PrincipalLabel'>
-                <label><p>Nome:</p></label><br/>
-
-                <label><p>Descricao:</p></label><br/>
-
-                <label><p>Preço:</p></label><br/>
-
-                <label><p>Imagem:</p></label><br/>
-                </div>
-                
-                
-                
-                <div id='form_Modal_PrincipalInput'>
-                <input className='input_Modal_AddProduto'
-                type='text'
-                value={nome}
-                onChange={(e)=>setNome(e.target.value)}
-                /><br/>
-
-                <input className='input_Modal_AddProduto'
-                type='text'
-                value={descricao}
-                onChange={(e)=>setDescricao(e.target.value)}
-                /><br/>
-
-                <input className='input_Modal_AddProduto'
-                type='number'
-                value={preco}
-                onChange={(e)=>setPreco(e.target.value)}
-                />
-                <br/>
-
-                <input className='input_Modal_AddProduto'
-                type='file'
-                accept='image/jpeg, image/png'
-                onChange={handleImagem}
-                /><br/>
-
-                </div>
-                </div>
-                <button
-                type='submit' id='button_ModalAdd'
-                >Adicionar Produto</button>
-
-            </form>
+                <DashListarProdutos />
             </div>
-            </main>
-            
-            </Modal>
-            </div>
-            <div>
-                <DashListarProdutos/>
-            </div>
-            
+
         </section>
     )
 }
