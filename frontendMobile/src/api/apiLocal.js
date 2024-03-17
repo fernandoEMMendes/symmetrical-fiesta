228dd51858ee448a23832777dc0c958aaa6a3ddd
Luizinho05 <luizhsimionato@gmail.com>
2024-03-17 12:15:26 -0300

    a

--- frontendMobile/src/api/apiLocal.js
+++ frontendMobile/src/api/apiLocal.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 const apiLocal = axios.create({
-  baseURL: "http://10.152.46.23:7600",
+  baseURL: "http://192.168.0.115:7600",
 });
 
 export default apiLocal;
 
 //192.168.0.208 PC YEMURI,
 //10.152.46.24 PC SENAC LUIZ,
-//192.168.0.51 PC LOCAL LUIZ
+//192.168.0.115 PC LOCAL LUIZ
 //10.152.46.35 PC SENAC FERNANDO
